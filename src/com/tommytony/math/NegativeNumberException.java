6dc77bca96af7d2652e8bda401e3f34fbe0b5d19
grinning <grin@tommytony.com>
2012-08-29 02:43:57 -0400

    Negative Number Exception for Math

---
+++ src/com/tommytony/math/NegativeNumberException.java
@@ -0,0 +1,10 @@
+package com.tommytony.math;
+
+public class NegativeNumberException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7898898363628139184L; 
+
+}
