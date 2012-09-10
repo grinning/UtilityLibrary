fdc49dfd3da074513eab55e115b12061321ed666
Jon Becker Ewing <jbewing@Jons-MacBook-Pro.local>
2012-09-09 21:36:00 -0400

    Small Bug fixes here and there

--- src/com/tommytony/io/Deflate.java
+++ src/com/tommytony/io/Deflate.java
@@ -20,7 +20,7 @@ public class Deflate {
 		ZipInputStream zipin = new ZipInputStream(new BufferedInputStream(new FileInputStream(inputFile)));
 		ZipEntry entry;
 		while((entry = zipin.getNextEntry()) != null) {
-			int count;
+			int count = 0;
 			byte[] data = new byte[Deflate.BUFFER_SIZE];
 			dest = new BufferedOutputStream(new FileOutputStream(entry.getName()), Deflate.BUFFER_SIZE);
 			while((count = zipin.read(data, 0, Deflate.BUFFER_SIZE)) != -1) {
@@ -34,7 +34,7 @@ public class Deflate {
 	
 	public static void gZip(File inputFile, String outputFile) throws IOException {
 		BufferedOutputStream dest = null;
-		int count;
+		int count = 0;
 		GZIPInputStream zipin = new GZIPInputStream(new BufferedInputStream(new FileInputStream(inputFile)));
 		byte[] data = new byte[Deflate.BUFFER_SIZE];
 		dest = new BufferedOutputStream(new FileOutputStream(outputFile), Deflate.BUFFER_SIZE);
--- src/com/tommytony/io/Encrypt.java
+++ src/com/tommytony/io/Encrypt.java
@@ -14,19 +14,18 @@ import javax.crypto.spec.SecretKeySpec;
 
 public class Encrypt {
 
-	public static String encryptWithAES(String data, String key) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
-		byte[] keyBytes = key.getBytes();
-		if(keyBytes.length < 16) {
-			for(int i = 0; i < (16 - keyBytes.length); i++) {
-				keyBytes[keyBytes.length + i] = keyBytes[i];
+	public static String encryptWithAES(String data, byte[] key) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+		if(key.length < 16) {
+			for(int i = 0; i < (16 - key.length); i++) {
+				key[key.length + i] = key[i];
 			}
-		} else if(keyBytes.length > 16) {
-			for(int i = 0; keyBytes.length != 16; i--) {
-				keyBytes[keyBytes.length + i] = (Byte) null;
+		} else if(key.length > 16) {
+			for(int i = 0; key.length != 16; i--) {
+				key[key.length + i] = (Byte) null;
 			}
 		}
 		Cipher aes = Cipher.getInstance("AES");
-		SecretKeySpec keymaker = new SecretKeySpec(keyBytes, "AES");
+		SecretKeySpec keymaker = new SecretKeySpec(key, "AES");
 		aes.init(Cipher.ENCRYPT_MODE, keymaker);
 		return new String(aes.doFinal(data.getBytes()));
 	}
